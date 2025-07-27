ebbb51a10c44270cf9687ba78dc957c7197e3689
alextalijan <acatalijan@icloud.com>
2025-07-27 16:03:11 +0200

    feat: write unit tests for length, # of hits and isSunk

---
+++ js/tests/Ship.test.js
@@ -0,0 +1,38 @@
+import Ship from '../Ship.js';
+
+describe('Ship Length', () => {
+  it('Is of inputed length', () => {
+    expect(new Ship(4).length).toBe(4);
+  });
+  it("Can't be of length less than one", () => {
+    expect(new Ship(0)).toThrow("Ship can't be of length less than one.");
+  });
+});
+
+const hitMock = jest.fn((ship) => (ship.hits += 1));
+
+describe('Number of hits', () => {
+  const testShip = new Ship(5);
+  it('Shows the number of hits as 0 when ship is created', () => {
+    expect(testShip.hits).toBe(0);
+  });
+
+  it('Increases hits by 1 when the ship has been hit', () => {
+    hitMock(testShip);
+    expect(testShip.hits).toBe(1);
+  });
+});
+
+describe('Has it been sunk', () => {
+  const testShip = new Ship(3);
+  it("Shows the ship hasn't sunk when it hadn't been", () => {
+    hitMock(testShip);
+    expect(testShip.isSunk).toBe(false);
+  });
+
+  it('Shows when the ship has been sunk', () => {
+    hitMock(testShip);
+    hitMock(testShip);
+    expect(testShip.isSunk).toBe(true);
+  });
+});
