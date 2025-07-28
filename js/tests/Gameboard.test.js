45844cc4b4d09fa5424f07b08a8e31342c257291
alextalijan <acatalijan@icloud.com>
2025-07-28 13:55:15 +0200

    feat: add tests for creating board and placing ships

---
+++ js/tests/Gameboard.test.js
@@ -0,0 +1,36 @@
+import Gameboard from '../Gameboard.js';
+
+describe('Initializes Gameboard', () => {
+  it('Gives an empty board when created', () => {
+    expect(new Gameboard().board[0][0]).toBe(null);
+    expect(new Gameboard().board[5][5]).toBe(null);
+  });
+  it('Gives an empty guessing board', () => {
+    expect(new Gameboard().guessingBoard[5][5]).toBe(null);
+    expect(new Gameboard().guessingBoard[10][10]).toBe(null);
+  });
+});
+
+describe('Placing ships', () => {
+  const testBoard = new Gameboard();
+  it("Places the ship horizontally if direction hasn't been specified", () => {
+    testBoard.placeShip(4, [0, 0]);
+    expect(testBoard.board[0][0]).toBe(true);
+    expect(testBoard.board[1][0]).toBe(false);
+  });
+  it('Places the ship vertically if it has been specified', () => {
+    testBoard.placeShip(3, [5, 5], 'vertical');
+    expect(testBoard.board[5][5]).toBe(true);
+    expect(testBoard.board[5][6]).toBe(false);
+    expect(testBoard.board[7][5]).toBe(true);
+  });
+  it('Places the ship of provided length', () => {
+    expect(testBoard.board[0][3]).toBe(true);
+    expect(testBoard.board[0][4]).toBe(false);
+  });
+  it("Can't place a ship outside of the board", () => {
+    expect(() => testBoard.placeShip([0, 11])).toThrow(
+      "Can't place a ship out of bounds."
+    );
+  });
+});
