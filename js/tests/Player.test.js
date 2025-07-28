276ad427e57851d27f911fc86ded158e301ba9e4
alextalijan <acatalijan@icloud.com>
2025-07-28 18:10:14 +0200

    feat: write tests for the Player class

---
+++ js/tests/Player.test.js
@@ -0,0 +1,22 @@
+import Player from '../Player.js';
+
+describe('Types of players', () => {
+  it('Creates a real player', () => {
+    expect(new Player('real').type).toBe('real');
+  });
+  it('Creates a computer player', () => {
+    expect(new Player('computer').type).toBe('computer');
+  });
+  it('Rejects to create any other player', () => {
+    expect(() => new Player('fake').type).toThrow(
+      'You can only chose "real" or "computer" players.'
+    );
+  });
+});
+
+describe('Own gameboard', () => {
+  it('Creates a new gameboard when a player is generated.', () => {
+    const player = new Player('real');
+    expect(player).toHaveProperty('gameboard');
+  });
+});
