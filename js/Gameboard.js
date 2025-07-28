cbb4de35290928e40e7193c9549dc74859558b93
alextalijan <acatalijan@icloud.com>
2025-07-28 14:49:25 +0200

    feat: add logic for checking out of bounds and overlapping ships
    Instead of just placing the ship, the functions now check if the full
    ship will make it inside the board, and also not overlap an existing one.

--- js/Gameboard.js
+++ js/Gameboard.js
@@ -11,28 +11,64 @@ class Gameboard {
     }
   }
 
-  placeShip(length, coordinates, direction = 'horizontal') {
+  placeShip(length, startingPoint, direction = 'horizontal') {
     if (
-      coordinates[0] < 0 ||
-      coordinates[0] > 9 ||
-      coordinates[1] < 0 ||
-      coordinates[1] > 9
+      startingPoint[0] < 0 ||
+      startingPoint[0] > 9 ||
+      startingPoint[1] < 0 ||
+      startingPoint[1] > 9
     ) {
       throw new Error("Can't place a ship out of bounds.");
     }
 
     const ship = new Ship(length);
     if (direction === 'horizontal') {
-      for (let j = coordinates[0]; length > 0; j += 1) {
-        this.board[coordinates[0]][j] = ship;
+      if (startingPoint[1] + length > 10) {
+        throw new Error('This ship surpasses the board and cannot be placed.');
+      }
+
+      if (this.#areThereShips(length, startingPoint, direction)) {
+        throw new Error('Cannot place a ship over another one.');
+      }
+
+      for (let j = startingPoint[1]; length > 0; j += 1) {
+        this.board[startingPoint[0]][j] = ship;
         length -= 1;
       }
     } else if (direction === 'vertical') {
-      for (let i = coordinates[0]; length > 0; i += 1) {
-        this.board[i][coordinates[1]] = ship;
+      if (startingPoint[0] + length > 10) {
+        throw new Error('This ship is too big to fit on the board.');
+      }
+
+      if (this.#areThereShips(length, startingPoint, direction)) {
+        throw new Error('Cannot place a ship over another one.');
+      }
+
+      for (let i = startingPoint[0]; length > 0; i += 1) {
+        this.board[i][startingPoint[1]] = ship;
         length -= 1;
       }
     }
+  }
+
+  #areThereShips(length, startingPoint, direction) {
+    if (direction === 'horizontal') {
+      for (let j = startingPoint[1]; length > 0; j += 1) {
+        if (this.board[startingPoint[0]][j] !== undefined) {
+          return true;
+        }
+        length -= 1;
+      }
+    } else {
+      for (let i = startingPoint[0]; length > 0; i += 1) {
+        if (this.board[i][startingPoint[1]] !== undefined) {
+          return true;
+        }
+        length -= 1;
+      }
+    }
+
+    return false;
   }
 }
 
