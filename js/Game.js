aa125968e8985f06eaed63eab51086356d3ed9ec
alextalijan <acatalijan@icloud.com>
2025-07-29 18:14:43 +0200

    feat: write logic for running rounds and choosing moves
    The Game object will run as long as the game is not over for the turn
    player, in which case the oponent won.

--- js/Game.js
+++ js/Game.js
@@ -40,4 +40,38 @@ const Game = function thatRepresentsTheWholeGame(players) {
       }
     }
   }
+
+  let turnPlayer = players.unshift();
+  while (!turnPlayer.gameboard.isGameOver) {
+    // Get the player back in queue again
+    players.push(turnPlayer);
+
+    if (turnPlayer.type === 'real') {
+      const userInput = prompt(
+        'Which spot do you want to attack? E.g. 5,5 -> you would attack 6th row, 6th column.'
+      );
+
+      const move = userInput.split(',');
+      if (
+        move.length === 2 &&
+        typeof move[0] === 'number' &&
+        typeof move[1] === 'number'
+      ) {
+        // Only if the move hasn't been made
+        if (
+          ![true, false].includes(turnPlayer.guessingBoard[move[0]][move[1]])
+        ) {
+          // Make an attack on the opponents ship
+          turnPlayer.guessingBoard[move[0]][move[1]] =
+            players[0].gameboard.receiveAttack(move);
+
+          turnPlayer = players.unshift();
+        } else {
+          alert('That move has already been made. Please choose another.');
+        }
+      } else {
+        alert('Please input the move in a valid format -> x,y');
+      }
+    }
+  }
 };
