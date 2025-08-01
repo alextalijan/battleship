7b1ea676909d12c941bbf54aa5ce858549d0a987
alextalijan <acatalijan@icloud.com>
2025-08-01 14:54:00 +0200

    feat: add form constraint validation and fix bugs
    User experience is better now due to custom validation message popping up.
    I've also fixed bugs that I have created introducing validation, so now
    the game starts properly and has validation.

--- js/newGame.js
+++ js/newGame.js
@@ -5,28 +5,36 @@ const newGame = function thatCreatesANewGame() {
   const newGameModal = document.querySelector('.new-game-modal');
   newGameModal.showModal();
 
-  const startGameBtn = document.querySelector('.start-game-btn');
-  startGameBtn.addEventListener('click', (event) => {
+  const newGameForm = document.querySelector('.new-game-form');
+  newGameForm.onsubmit = (event) => {
     event.preventDefault();
 
-    const playerOneType = document.querySelector(
-      'input[name="playerOne"]:checked'
-    );
+    // Query the first player type if checked, otherwise capture the first one for validation message
+    let playerOneType =
+      document.querySelector('input[name="playerOne"]:checked') ||
+      document.querySelector('#playerOne');
+    playerOneType.setCustomValidity('');
+
     const playerTwoType = document.querySelector(
       'input[name="playerTwo"]:checked'
     );
 
-    if (!playerOneType || !playerTwoType) {
-      alert('Please choose player types for both players.');
+    if (!playerOneType.validity.valid) {
+      playerOneType.setCustomValidity(
+        'You must choose a player type for the first player.'
+      );
+      playerOneType.reportValidity();
     } else {
       newGameModal.close();
+      newGameForm.reset();
+      newGameForm.removeAttribute('onsubmit');
 
       // Create both players and start their game
       const playerOne = new Player(playerOneType.value);
-      const playerTwo = new Player(playerTwoType.value);
+      const playerTwo = new Player(playerTwoType.value || 'computer');
       Game([playerOne, playerTwo]);
     }
-  });
+  };
 };
 
 export default newGame;
