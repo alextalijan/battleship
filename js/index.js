f358bfec8d1b77d27936d62dd47facbcf3782f1a
alextalijan <acatalijan@icloud.com>
2025-08-01 15:31:35 +0200

    feat: add button for closing the modal voluntarily

--- js/index.js
+++ js/index.js
@@ -4,3 +4,10 @@ const newGameBtn = document.querySelector('.create-new-game-btn');
 newGameBtn.addEventListener('click', () => {
   newGame();
 });
+
+// Initializes close button for new game modal
+const modal = document.querySelector('.new-game-modal');
+const closeBtn = document.querySelector('.modal-close-btn');
+closeBtn.addEventListener('click', () => {
+  modal.close();
+});
