8b659cc1a15c1fd4e6175f95b2e5f069b0a8008b
alextalijan <acatalijan@icloud.com>
2025-08-08 18:49:37 +0200

    feat: add option to input player names
    When the radio button for a real player is selected, label turns into
    an input field that accepts text, and gives it as name to the relevant
    player.

--- js/index.js
+++ js/index.js
@@ -15,10 +15,46 @@ newGameBtn.addEventListener('click', () => {
     newGameModal.close();
   });
 
+  // If the user chooses player as type, he gets to select his name
+  const radioButtons = [
+    ...document.querySelectorAll('input[name="playerOne"]'),
+    ...document.querySelectorAll('input[name="playerTwo"]'),
+  ];
+  radioButtons.forEach((button) => {
+    button.addEventListener('change', (event) => {
+      let label, nameInput;
+
+      if (event.target.value === 'real') {
+        label = document.querySelector(
+          `input[name="${event.target.id}"]`
+        ).nextElementSibling;
+
+        const nameInput = document.createElement('input');
+        nameInput.classList.add(`${event.target.id}NameInput`);
+        nameInput.type = 'text';
+        nameInput.value = label.innerText;
+
+        // Change the label for the input field
+        label.parentNode.replaceChild(nameInput, label);
+      } else {
+        nameInput =
+          event.target.parentNode.previousElementSibling.querySelector(
+            'input[type="text"]'
+          );
+
+        label = document.createElement('label');
+        label.textContent = 'Player';
+
+        nameInput.parentNode.replaceChild(label, nameInput);
+      }
+    });
+  });
+
   const newGameForm = document.querySelector('.new-game-form');
   newGameForm.onsubmit = (event) => {
     event.preventDefault();
 
+    // TODO: add support for capturing player name
     // Query the first player type if checked, otherwise capture the first one for validation message
     let playerOneType =
       document.querySelector('input[name="playerOne"]:checked') ||
@@ -36,12 +72,26 @@ newGameBtn.addEventListener('click', () => {
       );
       playerOneType.reportValidity();
     } else {
+      const playerOneName = document.querySelector('.playerOneNameInput');
+      const playerTwoName = document.querySelector('.playerTwoNameInput');
+
       newGameModal.close();
       newGameForm.reset();
 
       // Create both players and start their game
-      const playerOne = new Player(playerOneType.value);
-      const playerTwo = new Player(playerTwoType.value);
+      let playerOne, playerTwo;
+      if (playerOneName) {
+        playerOne = new Player(playerOneType.value, playerOneName.value);
+      } else {
+        playerOne = new Player(playerOneType.value);
+      }
+
+      if (playerTwoName) {
+        playerTwo = new Player(playerTwoType.value, playerTwoName.value);
+      } else {
+        playerOne = new Player(playerTwoType.value);
+      }
+
       Game.newGame([playerOne, playerTwo]);
     }
   };
